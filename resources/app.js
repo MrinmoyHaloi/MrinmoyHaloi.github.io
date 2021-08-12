9afdc57bc590817fe4cb401ca96b2047e5e6fab7
MrinmoyHaloi <mrinmoyhaloi4@gmail.com>
2021-08-12 22:51:38 +0530

    major redesign

---
+++ resources/app.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.element', {
+    strings: ["",'Web developer', 'Software engineer','Freelancer','Graphic designer','Video Editor' ],
+    typeSpeed: 80,
+    backSpeed:10,
+    loop:true,
+  });
