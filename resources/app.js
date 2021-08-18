12581c6077bdf6513c4ff82d477543cbb45d1e2a
MrinmoyHaloi <mrinmoyhaloi4@gmail.com>
2021-08-18 19:28:45 +0530

    changed progress bar and navbar animation

--- resources/app.js
+++ resources/app.js
@@ -1,6 +1,27 @@
-
 var to_top = document.getElementById("to-top");
 var navbar = document.getElementById("navbar");
+var progress_bar = document.getElementsByClassName("progress-bar")
+var body = document.getElementsByTagName("body");
+
+// function func() {
+//   if (window.pageYOffset >= 90){
+//       progress_bar[0].style.width = "300px";
+//       progress_bar[1].style.width = "300px";
+//       progress_bar[2].style.width = "300px";
+//   }else{
+//       for(var i = 0; i < divs.length; i++) {
+//           divs[i].style.width = "70px"
+// }}
+function func() {
+  if (window.pageYOffset >= 500){
+    progress_bar[0].style.width = `${progress_bar[0].getAttribute("aria-valuenow")}%`;
+    progress_bar[1].style.width = `${progress_bar[1].getAttribute("aria-valuenow")}%`;
+    progress_bar[2].style.width = `${progress_bar[2].getAttribute("aria-valuenow")}%`;
+    progress_bar[3].style.width = `${progress_bar[3].getAttribute("aria-valuenow")}%`;
+    progress_bar[4].style.width = `${progress_bar[4].getAttribute("aria-valuenow")}%`;
+  }
+}
+
 
 function myFunction() {
   if (window.pageYOffset <= 90) {
@@ -8,10 +29,10 @@ function myFunction() {
   } else {
     to_top.classList.remove("to-top-invisible");
   }
-  if (window.pageYOffset <= 100){
+  if (window.pageYOffset <= 370) {
     navbar.classList.add("pinned-nav");
-  } else{
-    navbar.classList.remove("pinned-nav")
+  } else {
+    navbar.classList.remove("pinned-nav");
   }
 }
 var typed = new Typed(".element", {
@@ -26,4 +47,4 @@ var typed = new Typed(".element", {
   typeSpeed: 80,
   backSpeed: 10,
   loop: true,
-});
+})
