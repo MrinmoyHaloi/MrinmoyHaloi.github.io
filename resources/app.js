17e27cc09b43946eae262521359f764c8eee1e1b
mrinmoyhaloi <mrinmoyhaloi4@gmail.com>
2021-12-07 17:33:58 +0530

    formatted html and corrected twitter link

--- resources/app.js
+++ resources/app.js
@@ -7,37 +7,37 @@ const date = new Date();
 document.getElementById("year").innerHTML = date.getFullYear();
 
 function animate_progress() {
-  if (window.scrollY >= 450){
-    progress_bar.forEach(element => {
-        element.style.width = `${element.getAttribute("aria-valuenow")}%`;
-    });
-  }
+    if (window.scrollY >= 450) {
+        progress_bar.forEach(element => {
+            element.style.width = `${element.getAttribute("aria-valuenow")}%`;
+        });
+    }
 }
 
 //changes the navbar size and makes the to top button visible on scroll
 function scrollEvent() {
-  if (window.scrollY <= 90) {
-    to_top.classList.add("to-top-invisible");
-  } else {
-    to_top.classList.remove("to-top-invisible");
-  }
-  if (window.scrollY <= 370) {
-    navbar.classList.add("pinned-nav");
-  } else {
-    navbar.classList.remove("pinned-nav");
-  }
+    if (window.scrollY <= 90) {
+        to_top.classList.add("to-top-invisible");
+    } else {
+        to_top.classList.remove("to-top-invisible");
+    }
+    if (window.scrollY <= 370) {
+        navbar.classList.add("pinned-nav");
+    } else {
+        navbar.classList.remove("pinned-nav");
+    }
 }
 let prefix_typedjs = "I am a "
 let typed = new Typed(".element", {
-  strings: [
-    `${prefix_typedjs} Web developer`,
-    `${prefix_typedjs} Software developer`,
-    `${prefix_typedjs} Freelancer`,
-    `${prefix_typedjs} Graphics designer`,
-    `${prefix_typedjs} Video Editor`,
-  ],
-  typeSpeed: 80,
-  backSpeed: 10,
-  loop: true,
-  smartBackspace:true
+    strings: [
+        `${prefix_typedjs} Web developer`,
+        `${prefix_typedjs} Software developer`,
+        `${prefix_typedjs} Freelancer`,
+        `${prefix_typedjs} Graphics designer`,
+        `${prefix_typedjs} Video Editor`,
+    ],
+    typeSpeed: 80,
+    backSpeed: 10,
+    loop: true,
+    smartBackspace: true
 })
